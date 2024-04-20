c6e72614099d153d763f0137752093b357504276
oriolmontcreus <licensedgamer641319@gmail.com>
2024-04-20 21:09:56 +0200

    hero highlight style changes, passed to underline animation

--- src/app/ui/hero.tsx
+++ src/app/ui/hero.tsx
@@ -1,64 +1,7 @@
 "use client";
 import { cn } from "../../../utils/cn";
-import { useMotionValue, motion, useMotionTemplate } from "framer-motion";
+import { motion } from "framer-motion";
 import React from "react";
-
-export const HeroHighlight = ({
-  children,
-  className,
-  containerClassName,
-}: {
-  children: React.ReactNode;
-  className?: string;
-  containerClassName?: string;
-}) => {
-  let mouseX = useMotionValue(0);
-  let mouseY = useMotionValue(0);
-
-  function handleMouseMove({
-    currentTarget,
-    clientX,
-    clientY,
-  }: React.MouseEvent<HTMLDivElement>) {
-    if (!currentTarget) return;
-    let { left, top } = currentTarget.getBoundingClientRect();
-
-    mouseX.set(clientX - left);
-    mouseY.set(clientY - top);
-  }
-  return (
-    <div
-      className={cn(
-        "relative h-full flex items-center bg-white dark:bg-black justify-center w-full group",
-        containerClassName
-      )}
-      onMouseMove={handleMouseMove}
-    >
-      <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800  pointer-events-none" />
-      <motion.div
-        className="pointer-events-none bg-dot-thick-blue-500 dark:bg-dot-thick-blue-500 absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
-        style={{
-          WebkitMaskImage: useMotionTemplate`
-            radial-gradient(
-              200px circle at ${mouseX}px ${mouseY}px,
-              black 0%,
-              transparent 100%
-            )
-          `,
-          maskImage: useMotionTemplate`
-            radial-gradient(
-              200px circle at ${mouseX}px ${mouseY}px,
-              black 0%,
-              transparent 100%
-            )
-          `,
-        }}
-      />
-
-      <div className={cn("relative z-20", className)}>{children}</div>
-    </div>
-  );
-};
 
 export const Highlight = ({
   children,
@@ -70,10 +13,10 @@ export const Highlight = ({
   return (
     <motion.span
       initial={{
-        backgroundSize: "0% 100%",
+        backgroundSize: "0% 2px",
       }}
       animate={{
-        backgroundSize: "100% 100%",
+        backgroundSize: "100% 2px",
       }}
       transition={{
         duration: 0.5,
@@ -82,11 +25,11 @@ export const Highlight = ({
       }}
       style={{
         backgroundRepeat: "no-repeat",
-        backgroundPosition: "left center",
+        backgroundPosition: "left bottom",
         display: "inline",
       }}
       className={cn(
-        `relative inline-block pb-1 px-1 rounded-lg bg-gradient-to-r from-gray-700 to-gray-800 dark:from-gray-800 dark:to-gray-900`,
+        `relative inline-block pb-1 px-1 rounded-lg bg-gradient-to-r from-green-100/50 to-green-200/50 dark:from-green-500/50 dark:to-green-600/50`,
         className
       )}
     >
