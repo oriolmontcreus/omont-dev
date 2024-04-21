7743e9d69c8a45aadbc0e4b56ad95d4fd8a26f31
oriolmontcreus <licensedgamer641319@gmail.com>
2024-04-21 17:05:50 +0200

    basic working point of ExperienceItem timeline

--- src/app/experiencia/page.tsx
+++ src/app/experiencia/page.tsx
@@ -8,18 +8,48 @@ const EXPERIENCES = [
     description:
       "Responsable de la creación y actualización de herramientas web internas, mejorando la eficiencia y productividad de la empresa. Mis tareas incluyen el diseño y desarrollo de interfaces de usuario, la implementación de nuevas funcionalidades y la resolución de problemas técnicos para garantizar un rendimiento óptimo.",
     imageUrl: "/ExperienceLogos/irontech-whitebg.webp",
+    isDevRelated: true,
+  },
+  {
+    date: "Julio 2023 - Agosto 2023",
+    title: "Cartero / Repartidor motarizado - Correos",
+    description:
+      "Preparar rutas de reparto, organizar y repartir correo ordinario, votos por correo, paquetería y correo certificado. Repartidor en Bonmatí, Sant Julià de Llor, Osor, La Cellera de Ter, Anglès i Vidreres.",
+    imageUrl: "/ExperienceLogos/correos.webp",
+    isDevRelated: false,
+  },
+  {
+    date: "Julio 2023 - Agosto 2023",
+    title: "Cartero / Repartidor motarizado - Correos",
+    description:
+      "Preparar rutas de reparto, organizar y repartir correo ordinario, votos por correo, paquetería y correo certificado. Repartidor en Bonmatí, Sant Julià de Llor, Osor, La Cellera de Ter, Anglès i Vidreres.",
+    imageUrl: "/ExperienceLogos/correos.webp",
+    isDevRelated: false,
   },
 
 ];
 
 export function ExperiencePage() {
+    const developerExperiences = EXPERIENCES.filter(experience => experience.isDevRelated);
+    const otherExperiences = EXPERIENCES.filter(experience => !experience.isDevRelated);
+
     return (
         <section id="experience" data-section="experience" className="relative flex flex-col h-screen dark max-w-2xl mx-auto pt-20">
             <h2 className="flex items-center mb-6 text-3xl font-semibold gap-x-3 text-black/80 dark:text-white">
                 <ExperienceIcon />
                 Experiencia laboral
             </h2>
-            {EXPERIENCES.map((experience, index) => (
+            {developerExperiences.map((experience, index) => (
+                <div className="relative" key={index}>
+                    <ExperienceItem {...experience} />
+                </div>
+            ))}
+            <div className="my-6 border-t border-gray-200 dark:border-gray-700 text-center relative">
+            <span className="absolute left-0 right-0 top-50% transform -translate-y-50% text-gray-500 font-semibold">
+              Roles en otros campos
+            </span>
+            </div>
+            {otherExperiences.map((experience, index) => (
                 <div className="relative" key={index}>
                     <ExperienceItem {...experience} />
                 </div>
--- src/app/ui/experience-item.tsx
+++ src/app/ui/experience-item.tsx
@@ -5,21 +5,22 @@ interface Props {
     description: string;
     date: string;
     imageUrl: string;
+    isDevRelated: boolean;
 }
 
 const ExperienceItem: React.FC<Props> = ({ title, description, date, imageUrl }) => {
     return (
         <div className="flex flex-col mb-2">
-            <img src={imageUrl} alt={title} style={{borderRadius: '7px', width: '100px', height: 'auto', objectFit: 'cover', marginBottom: '10px'}} />
             <div className="flex">
                 <div className="flex flex-col mr-4">
-                    <div className="w-3 h-3 rounded-full bg-gray-700 border-2 border-gray-900"></div>
-                    <div className="w-0.5 h-full bg-gray-300 mt-2 relative">
+                    <div className="w-3 h-3 rounded-full bg-gray-700 border-2 border-gray-900 absolute" style={{ top: '-7px', left: '-5px' }}></div>
+                    <div className="w-0.5 bg-gray-300 mt-1.5 relative" style={{ height: '100%' }}>
                         <div className="absolute top-0 left-0 w-0.5 h-0.5 rounded-full bg-gray-300"></div>
                         <div className="absolute bottom-0 left-0 w-0.5 h-0.5 rounded-full bg-gray-300"></div>
                     </div>
                 </div>
                 <div>
+                    <img src={imageUrl} alt={title} style={{ borderRadius: '7px', width: '48px', height: '48px', objectFit: 'cover', marginBottom: '10px' }} />
                     <time className="block mb-1 text-sm font-normal leading-none text-sky-800 dark:text-sky-200/80">{date}</time>
                     <h3 className="text-lg font-semibold text-yellow-800 dark:text-yellow-200 mt-2">{title}</h3>
                     <p className="mb-4 text-base font-normal dark:text-gray-200 text-pretty">{description}</p>
