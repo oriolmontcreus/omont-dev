fc4eea53b1123dddc3e5602ca5c3c3e15a5549bd
oriolmontcreus <licensedgamer641319@gmail.com>
2024-04-25 21:20:48 +0200

    feat: added about me content and added more flexible layout

--- src/app/sobre-mi/page.tsx
+++ src/app/sobre-mi/page.tsx
@@ -46,7 +46,7 @@ function EmailContact({ email }: EmailContactProps) {
   return (
     <div className="mt-4 p-2 border border-gray-300 dark:border-gray-700 rounded-md bg-gray-100 dark:bg-gray-800 flex items-center">
       <p className="text-neutral-500 text-sm dark:text-neutral-300 mr-2 flex-grow">
-        <a href={`mailto:${email}`}>{email}</a>
+        <p>{email}</p>
       </p>
       <LinkButton
         href={`mailto:${email}`}
@@ -69,39 +69,54 @@ export function ExperiencePage() {
         Sobre mí
       </h2>
 
-      <CardContainer className="inter-var">
-        <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-          <CardItem translateZ="100" className="w-full mt-4 flex flex-col items-center justify-center">
-            <Image
-              src="/personal/oriolmontcreus.webp"
-              height="1000"
-              width="1000"
-              className="h-full w-full object-cover rounded-xl group-hover/card:shadow-xl"
-              alt="profile-picture"
-            />
-            <CardItem
-              as="p"
-              translateZ="60"
-              className="text-neutral-500 text-sm max-w-60 mt-2 dark:text-neutral-300"
-            >
-              <ul className="list-disc list-inside space-y-2">
-                <li>🚹 Oriol Mont Creus</li>
-                <li>🎂 06 - 2004</li>
-                <li>💻 Desarrollador Full-stack</li>
-              </ul>
-            </CardItem>
-          </CardItem>
-          <div className="flex justify-center items-center w-full mt-10 gap-5">
-            <LinkButton href="https://www.linkedin.com/in/oriol-mont-creus/" className="hover:bg-gray-900 hover:border-gray-900">
-              <LinkedInIcon /> LinkedIn
-            </LinkButton>
-            <LinkButton href="https://github.com/oriolmontcreus" className="hover:bg-gray-800 hover:border-gray-800">
-              <GitHubIcon /> GitHub
-            </LinkButton>
+      <div className="flex flex-col md:flex-row justify-between items-start">
+        <div className="w-full md:w-1/2 p-4">
+          <div className="space-y-4">
+            <p className="leading-relaxed">
+              Mi nombre es <strong className="text-yellow-500"> Oriol Mont</strong>, un joven de 19 años originario de Girona. Soy un <strong className="text-yellow-500">Desarrollador FullStack</strong> y en la actualidad, estoy ampliando mis habilidades técnicas aprendiendo nuevos frameworks web, incluyendo <strong className="text-yellow-500">Angular, Laravel, Astro y Blazor</strong>.
+            </p>
+              <p className="leading-relaxed">
+                Mi pasión radica en optimizar el rendimiento y la estética de las aplicaciones web para proporcionar la <strong className="text-yellow-500">mejor experiencia al usuario</strong>. Creo firmemente en la importancia de un <strong className="text-yellow-500">diseño intuitivo</strong> y una <strong className="text-yellow-500">interacción fluida</strong> para el éxito de cualquier proyecto web.
+              </p>
           </div>
-          <EmailContact email="oriolmontcreus@gmail.com" />
-        </CardBody>
-      </CardContainer>
+        </div>
+
+        <div className="relative md:absolute right-0 w-full md:w-1/2 h-full">
+          <CardContainer className="inter-var h-full">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+            <CardItem translateZ="100" className="w-full flex flex-col items-center justify-center">
+              <Image
+                src="/personal/oriolmontcreus.webp"
+                height="1000"
+                width="1000"
+                className="h-full w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                alt="profile-picture"
+              />
+              <CardItem
+                as="p"
+                translateZ="60"
+                className="text-neutral-500 text-sm max-w-60 mt-2 dark:text-neutral-300"
+              >
+                <ul className="list-disc list-inside space-y-2">
+                  <li>🚹 Oriol Mont Creus</li>
+                  <li>🎂 06 - 2004</li>
+                  <li>💻 Desarrollador Full-stack</li>
+                </ul>
+              </CardItem>
+            </CardItem>
+            <div className="flex justify-center items-center w-full mt-10 gap-5">
+              <LinkButton href="https://www.linkedin.com/in/oriol-mont-creus/" className="hover:bg-gray-900 hover:border-gray-900">
+                <LinkedInIcon /> LinkedIn
+              </LinkButton>
+              <LinkButton href="https://github.com/oriolmontcreus" className="hover:bg-gray-800 hover:border-gray-800">
+                <GitHubIcon /> GitHub
+              </LinkButton>
+            </div>
+            <EmailContact email="oriolmontcreus@gmail.com" />
+          </CardBody>
+        </CardContainer>
+      </div>
+      </div>
     </section>
   );
 }
