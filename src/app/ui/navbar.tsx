a10d82aaf3ea46f471ee3d7d83298be3e37212e5
oriolmontcreus <licensedgamer641319@gmail.com>
2024-05-25 21:49:40 +0200

    feat: added home icon inside the navbar

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -2,8 +2,10 @@ import { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
+
 import NavigationIcon from './icons/NavigationIcon';
 import CloseIcon from './icons/CloseIcon';
+import HomeIcon from './icons/HomeIcon';
 
 type LinkItem = {
   name: string;
@@ -39,7 +41,7 @@ const Navbar = () => {
   };
 
   return (
-  <nav className="bg-gray-900 bg-opacity-80 border border-gray-700 px-4 py-2 mt-4 rounded-2xl max-w-[42em] w-auto mx-auto z-50 relative backdrop-blur-md">
+    <nav className="bg-gray-900 bg-opacity-80 border border-gray-700 px-4 py-2 mt-4 rounded-2xl max-w-[42em] w-auto mx-auto z-50 relative backdrop-blur-md">
       {isOpen && isMobile && (
         <button
           type="button"
@@ -55,20 +57,30 @@ const Navbar = () => {
             {!isOpen && <NavigationIcon />}
           </button>
         </div>
-        <div className={`flex flex-col space-y-4 md:flex-row md:space-x-4 ${isOpen ? 'block' : 'hidden'} md:block`}>
-          {linkItems.map((item) => (
-            <Link key={item.name} href={item.url}>
+      <div className={`flex flex-col space-y-4 md:flex-row md:space-x-4 ${isOpen ? 'block' : 'hidden'} md:block`}>
+          <Link href="/">
               <motion.div
-                className={`cursor-pointer inline-block px-4 py-2 rounded-full border-2 border-transparent transition-all duration-200 ease-in-out ${path === item.url ? 'text-blue-500 hover:border-white' : 'text-white hover:text-gray-200  hover:border-white'}`}
-                variants={linkVariants}
-                initial="initial"
-                whileHover="hover"
+                  className={`cursor-pointer inline-block px-4 py-2 -mb-3.5 rounded-full border-2 border-transparent transition-all duration-200 ease-in-out items-center ${path === '/' ? 'text-blue-500 hover:border-red-500' : 'text-white hover:text-gray-200  hover:border-white'}`}
+                  variants={linkVariants}
+                  initial="initial"
+                  whileHover="hover"
               >
-                {item.name}
+                  <HomeIcon />
               </motion.div>
-            </Link>
+          </Link>
+          {linkItems.map((item) => (
+              <Link key={item.name} href={item.url}>
+                  <motion.div
+                      className={`cursor-pointer inline-block px-4 py-2 rounded-full border-2 border-transparent transition-all duration-200 ease-in-out ${path === item.url ? 'text-blue-500 hover:border-white' : 'text-white hover:text-gray-200  hover:border-white'}`}
+                      variants={linkVariants}
+                      initial="initial"
+                      whileHover="hover"
+                  >
+                      {item.name}
+                  </motion.div>
+              </Link>
           ))}
-        </div>
+      </div>
       </div>
     </nav>
   );
