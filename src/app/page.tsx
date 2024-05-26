ebf45521887f9589ecce54267da9db70cc335aa9
oriolmontcreus <licensedgamer641319@gmail.com>
2024-05-26 12:00:06 +0200

    fix: fix responsive view home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,59 +11,63 @@ import AboutMeIcon from "./ui/icons/AboutMeIcon";
 export function HomePage() {
 return (
   <div className="relative flex flex-col items-center justify-center h-screen dark max-w-2xl mx-auto">
-    <div className="w-full flex flex-row justify-start items-center mt-4">
-      <motion.h1
-        initial={{
-          opacity: 0,
-          y: 20,
-        }}
-        animate={{
-          opacity: 1,
-          y: [20, -5, 0],
-        }}
-        transition={{
-          duration: 0.5,
-          ease: [0.4, 0.0, 0.2, 1],
-        }}
+<div className="w-full flex flex-row justify-start items-center mt-4 flex-wrap">
+  <div>
+    <motion.h1
+      initial={{
+        opacity: 0,
+        y: 20,
+      }}
+      animate={{
+        opacity: 1,
+        y: [20, -5, 0],
+      }}
+      transition={{
+        duration: 0.5,
+        ease: [0.4, 0.0, 0.2, 1],
+      }}
       className="text-2xl px-4 md:text-4xl lg:text-1xl font-bold text-neutral-700 dark:text-white max-w-2xl leading-relaxed lg:leading-snug text-center mx-auto whitespace-nowrap"
-      >
+    >
       ¡Hola!
       soy{" "}
       <Highlight className="text-black dark:text-white">
         Oriol Mont
       </Highlight>
-      </motion.h1>
-      <motion.div
-        initial={{
-          opacity: 0,
-          y: 20,
-        }}
-        animate={{
-          opacity: 1,
-          y: [20, -5, 0],
-        }}
-        transition={{
-          delay: 1.5,
-          duration: 0.5,
-          ease: [0.4, 0.0, 0.2, 1],
-        }}
-        className="flex justify-start w-full"
-      >
-        <div className="mr-5 mt-2.5 rounded-full">
-          <HoverBorderGradient
-            containerClassName="rounded-full"
-            as="button"
-            className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
-            onClick={() => window.open('https://www.linkedin.com/in/oriol-mont-creus/', '_blank')}
-          >
-            <LinkedInLogo />
-            <span>Conectar</span>
-          </HoverBorderGradient>
-        </div>
-      </motion.div>
-    </div>
-    <SectionCard items={projects} />
+    </motion.h1>
   </div>
+  <div>
+    <motion.div
+      initial={{
+        opacity: 0,
+        y: 20,
+      }}
+      animate={{
+        opacity: 1,
+        y: [20, -5, 0],
+      }}
+      transition={{
+        delay: 1.5,
+        duration: 0.5,
+        ease: [0.4, 0.0, 0.2, 1],
+      }}
+      className="flex justify-start w-full"
+    >
+      <div className="mr-5 mt-2.5 rounded-full">
+        <HoverBorderGradient
+          containerClassName="rounded-full"
+          as="button"
+          className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+          onClick={() => window.open('https://www.linkedin.com/in/oriol-mont-creus/', '_blank')}
+        >
+          <LinkedInLogo />
+          <span>Conectar</span>
+        </HoverBorderGradient>
+      </div>
+    </motion.div>
+  </div>
+</div>
+<SectionCard items={projects} />
+</div>
 );
 }
 
