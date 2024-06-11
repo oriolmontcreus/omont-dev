39defe14067c8f9f3e30689305d2e3ada07bcf46
oriolmontcreus <licensedgamer641319@gmail.com>
2024-06-11 19:30:58 +0200

    content change: translated proyectos to spanish

--- src/app/proyectos/content.ts
+++ src/app/proyectos/content.ts
@@ -2,26 +2,26 @@ export const projectsData = [
     {
         image: "/projects/vistaqs.webp",
         title: "VistaQs",
-        description: "Fullstack web application for managing surveys.",
+        description: "Gestión eficiente de encuestas con creación, distribución y análisis.",
         githubUrl: "https://github.com/oriolmontcreus/vistaQS"
     },
     {
         image: "/projects/sound-sanbox.webp",
-        title: "Sound Playground",
-        description: "Simple drumpad, channel rack and sound upload.",
+        title: "Audio Sandbox",
+        description: "Experimenta con música mediante un drumpad y mezclador sencillo.",
         githubUrl: "https://github.com/oriolmontcreus/vanilla-audio-sandbox",
         previewUrl: "https://oriolmontcreus.github.io/vanilla-audio-sandbox/"
     },
     {
         image: "/projects/vscode-extension.webp",
-        title: "Code Extension",
-        description: "Basic VS Code image path manager extension.",
+        title: "Ext. VS Code",
+        description: "Gestiona rutas de imágenes en proyectos con esta extensión para VS Code.",
         githubUrl: "https://github.com/oriolmontcreus/imagepatheditor-vsce"
     },
     {
         image: "/projects/dragndrop.webp",
-        title: "Drag&Drop Sort",
-        description: "Basic VS Code image path manager extension.",
+        title: "DragNDrop",
+        description: "Organiza listas interactivamente con esta herramienta de arrastrar y soltar.",
         githubUrl: "https://github.com/oriolmontcreus/simple-dragNdrop",
         previewUrl: "https://oriolmontcreus.github.io/simple-dragNdrop/"
     }
