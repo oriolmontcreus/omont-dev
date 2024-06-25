6084b9acff32f28a8fbdfa9c0219fc4640eee73f
oriolmontcreus <licensedgamer641319@gmail.com>
2024-06-25 22:05:43 +0200

    fix: fixed build error

--- src/app/experiencia/page.tsx
+++ src/app/experiencia/page.tsx
@@ -3,7 +3,6 @@ import ExperienceItem from "../ui/experience-item";
 import Navbar from "../ui/navbar";
 import { DEVELOPER_EXPERIENCES, OTHER_EXPERIENCES } from "./content";
 import { motion } from "framer-motion";
-
 import ExperienceIcon from "@icons/ExperienceIcon";
 
 const pageVariants = {
@@ -11,7 +10,7 @@ const pageVariants = {
   visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
 };
 
-export function ExperiencePage() {
+const ExperiencePage = () => {
   return (
     <section id="experiencia" data-section="experiencia" className="relative flex flex-col h-screen dark max-w-2xl mx-auto pt-40">
       <div style={{ position: 'fixed', top: '20px', left: '50%', transform: 'translateX(-50%)', zIndex: 1000 }}>
@@ -22,27 +21,27 @@ export function ExperiencePage() {
         Experiencia laboral
       </h2>
       <motion.section
-      id="experiencia"
-      data-section="experiencia"
-      initial="hidden"
-      animate="visible"
-      variants={pageVariants}
-    >
-      {DEVELOPER_EXPERIENCES.map((experience, index) => (
-        <div className="relative" key={index}>
-          <ExperienceItem {...experience} />
+        id="experiencia"
+        data-section="experiencia"
+        initial="hidden"
+        animate="visible"
+        variants={pageVariants}
+      >
+        {DEVELOPER_EXPERIENCES.map((experience, index) => (
+          <div className="relative" key={index}>
+            <ExperienceItem {...experience} />
+          </div>
+        ))}
+        <div className="my-6 border-t border-gray-200 dark:border-gray-700 text-center relative">
+          <span className="absolute left-0 right-0 top-50% transform -translate-y-50% text-gray-500 font-semibold">
+            Roles en otros campos
+          </span>
         </div>
-      ))}
-      <div className="my-6 border-t border-gray-200 dark:border-gray-700 text-center relative">
-        <span className="absolute left-0 right-0 top-50% transform -translate-y-50% text-gray-500 font-semibold">
-          Roles en otros campos
-        </span>
-      </div>
-      {OTHER_EXPERIENCES.map((experience, index) => (
-        <div className="relative p-5" key={index}>
-          <ExperienceItem {...experience} />
-        </div>
-      ))}
+        {OTHER_EXPERIENCES.map((experience, index) => (
+          <div className="relative p-5" key={index}>
+            <ExperienceItem {...experience} />
+          </div>
+        ))}
       </motion.section>
     </section>
   );
